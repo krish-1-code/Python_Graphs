2635caad2609df3061dae477c1b12d589c7acef6
Krish-01-code <krishkushwaha.w@gmail.com>
2026-01-18 15:32:44 -0600

    Subplots

---
+++ Python_Graph6.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.array([1,2,3,4,5,6,7,8,9,10])
+
+figure, axes = plt.subplots(2,2)
+
+axes[0,0].plot(x,x*2)
+axes[0,0].set_title("x*2")
+axes[0,1].plot(x,x**3)
+axes[1,0].plot(x,x**4)
+axes[1,1].plot(x,x**5)
+plt.show()
