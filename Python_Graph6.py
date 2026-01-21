afaa33d2a42ce89572f73010aaacafa3dcef5f86
Krish-01-code <krishkushwaha.w@gmail.com>
2026-01-21 16:43:05 -0600

    6th graph

--- Python_Graph6.py
+++ Python_Graph6.py
@@ -1,13 +1,19 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-x = np.array([1,2,3,4,5,6,7,8,9,10])
+x = np.array([1,2,3,4,5,6,7,8])
 
 figure, axes = plt.subplots(2,2)
 
-axes[0,0].plot(x,x*2)
-axes[0,0].set_title("x*2")
+axes[0,0].plot(x,x**2)
+axes[0,0].set_title("x**2")
+
 axes[0,1].plot(x,x**3)
+axes[0,1].set_title("x**3")
+
 axes[1,0].plot(x,x**4)
+axes[0,1].set_title("x**4")
+
 axes[1,1].plot(x,x**5)
+axes[0,1].set_title("x**5")
 plt.show()
